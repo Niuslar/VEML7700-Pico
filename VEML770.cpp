6dd9fe50d7926f80c8cffc2f0229dbcea26cd97b
niuslar <niuslar@outlook.com>
2023-06-24 15:14:43 +0100

    Initial commit

---
+++ VEML770.cpp
@@ -0,0 +1,136 @@
+
+#include "VEML770.h"
+
+VEML7700::VEML7700()
+{
+}
+
+VEML7700::~VEML7700() {}
+
+void VEML7700::Init(uint8_t sdaPin, uint8_t sclPin, i2c_inst_t *i2cInstance, uint32_t baudRate)
+{
+    if (i2cInstance == nullptr)
+        return;
+
+    /* Setup I2C and GPIOs */
+    m_i2cX = i2cInstance;
+    i2c_init(i2cInstance, baudRate);
+    gpio_set_function(sdaPin, GPIO_FUNC_I2C);
+    gpio_set_function(sclPin, GPIO_FUNC_I2C);
+    gpio_pull_up(sdaPin);
+    gpio_pull_up(sclPin);
+
+    /* Setup VEML770 sensor */
+    /* Default values are
+     *  ALS GAIN = x1
+     *  ALS IT   = 100 ms
+     *  ALS PERS = 1
+     *  ALS INT EN = 0
+     *  ALS SD (shutdown) = 0 (Turn on when Init is called)
+     */
+
+    ALS_CONF_0_REG = 0;
+    WriteRegister(&ALS_CONF_0_COMMAND, ALS_CONF_0_REG);
+
+    /* Wait 2.5ms before the first reading */
+    sleep_us(2500);
+}
+
+float VEML7700::ReadLux()
+{
+    uint16_t wholeWhite = ReadRegister(&ALS_COMMAND);
+
+    return (float)wholeWhite;
+}
+
+void VEML7700::Enable(bool enable)
+{
+    /* Clean bits */
+    ALS_CONF_0_REG &= ~(1 << ALS_CONF_SD_BIT);
+
+    /* Set bits */
+    ALS_CONF_0_REG |= (enable << ALS_CONF_SD_BIT);
+
+    /* Write config */
+    WriteRegister(&ALS_CONF_0_COMMAND, ALS_CONF_0_REG);
+}
+
+void VEML7700::SetGain(gainValues_t newGain)
+{
+    /* Clean bits */
+    ALS_CONF_0_REG &= ~(3 << ALS_CONF_GAIN_BIT);
+
+    /* Set bits */
+    ALS_CONF_0_REG |= ((uint8_t)newGain << ALS_CONF_GAIN_BIT);
+
+    /* Write config */
+    WriteRegister(&ALS_CONF_0_COMMAND, ALS_CONF_0_REG);
+}
+
+void VEML7700::SetIntegrationTiming(integrationTime_t integrationTiming)
+{
+    /* Clean bits */
+    ALS_CONF_0_REG &= ~(15 << ALS_CONF_IT_BIT);
+
+    /* Set bits */
+    ALS_CONF_0_REG |= ((uint8_t)integrationTiming << ALS_CONF_IT_BIT);
+
+    /* Write config */
+    WriteRegister(&ALS_CONF_0_COMMAND, ALS_CONF_0_REG);
+}
+
+void VEML7700::SetPersistenceProtect(persProt_t protNumber)
+{
+    /* Clean bits */
+    ALS_CONF_0_REG &= ~(3 << ALS_CONF_PERS_BIT);
+
+    /* Set bits */
+    ALS_CONF_0_REG |= ((uint8_t)protNumber << ALS_CONF_PERS_BIT);
+
+    /* Write config */
+    WriteRegister(&ALS_CONF_0_COMMAND, ALS_CONF_0_REG);
+}
+
+void VEML7700::EnableInterrupt(bool enable)
+{
+    /* Clean bits */
+    ALS_CONF_0_REG &= ~(1 << ALS_CONF_INT_EN_BIT);
+
+    /* Set bits */
+    ALS_CONF_0_REG |= ((uint8_t)enable << ALS_CONF_INT_EN_BIT);
+
+    /* Write config */
+    WriteRegister(&ALS_CONF_0_COMMAND, ALS_CONF_0_REG);
+}
+
+void VEML7700::SetHighLimit(uint16_t highThreshold)
+{
+    WriteRegister(&ALS_WH_COMMAND, highThreshold);
+}
+
+void VEML7700::SetLowLimit(uint16_t lowThreshold)
+{
+    /* Write config */
+    WriteRegister(&ALS_WL_COMMAND, lowThreshold);
+}
+
+/* Private Methods */
+
+uint16_t VEML7700::ReadRegister(const uint8_t *regAddr)
+{
+    i2c_write_timeout_us(m_i2cX, m_deviceAddress, regAddr, 1, true, m_defaultTimeout);
+    i2c_read_timeout_us(m_i2cX, m_deviceAddress, m_rxBuffer, 2, false, m_defaultTimeout);
+
+    uint16_t data = (m_rxBuffer[1] << 8) | (m_rxBuffer[0] & 0xFF);
+    return data;
+}
+
+void VEML7700::WriteRegister(const uint8_t *regAddr, uint16_t value)
+{
+    const int messageLen = 3;
+    uint8_t message[messageLen]; // Include command and data to send
+    message[0] = *regAddr;
+    message[1] = value >> 8;    // MSB
+    message[2] = value && 0xFF; // LSB
+    i2c_write_timeout_us(m_i2cX, m_deviceAddress, message, messageLen, true, m_defaultTimeout);
+}
---
+++ VEML770.h
@@ -0,0 +1,135 @@
+/**
+ * @file VEML770.h
+ * @brief Adafruit VEML770 ambient light sensor (ALS) driver for Raspberry Pi Pico
+ * @author niuslar
+ * @date 20 Jun 2023
+ */
+
+#ifndef VEML770_H
+#define VEML770_H
+
+#include "hardware/i2c.h"
+#include "hardware/gpio.h"
+#include <stdint.h>
+
+#define ALS_CONF_GAIN_BIT   12
+#define ALS_CONF_IT_BIT     9
+#define ALS_CONF_PERS_BIT   5
+#define ALS_CONF_INT_EN_BIT 1
+#define ALS_CONF_SD_BIT     0
+
+class VEML7700
+{
+public:
+    VEML7700();
+    ~VEML7700();
+
+    /**
+     * @brief Initialise i2c peripherals and define GPIOs to use
+     * @param sdaPin GPIO Pin to be configured as SDA (Please refer to Pico's Pinout to check pins available)
+     * @param sclPin GPIO Pin to be configures as SCL
+     * @param i2cInstance i2c0 or i2c1
+     * @param baudRate 100000 (STANDART) or 400000 (FAST)
+     */
+    void Init(uint8_t sdaPin = PICO_DEFAULT_I2C_SDA_PIN, uint8_t sclPin = PICO_DEFAULT_I2C_SCL_PIN, i2c_inst_t *i2cInstance = i2c0, uint32_t baudRate = 100 * 1000);
+
+    /**
+     * @brief Read Lux levels
+     * @return Lux levels from 0 to 120k
+     * */
+    float ReadLux();
+
+    /**
+     * @brief Enable readings
+     * @param enable True to enable readings, false to shutdown ALS
+    */
+    void Enable(bool enable);
+
+    enum class gainValues_t
+    {
+        ALS_GAIN_X1     = 0,    // Gain x1
+        ALS_GAIN_X2     = 1,    // Gain x2
+        ALS_GAIN_X1_8   = 2,  // Gain x1/8
+        ALS_GAIN_X1_4   = 3   // Gain x1/4
+    };
+
+    /**
+     * @brief Set ALS gain 
+     * @param newGain Gain as defined in the gainValues_t enum class
+    */
+    void SetGain(gainValues_t newGain);
+
+    enum class integrationTime_t
+    {
+        ALS_IT_25MS     = 12,
+        ALS_IT_50MS     = 8,
+        ALS_IT_100MS    = 0,
+        ALS_IT_200MS    = 1,
+        ALS_IT_400MS    = 2,
+        ALS_IT_800MS    = 3
+    };
+
+    /**
+     * @brief Set ALS integration timing (how long the sensor will capture photons)
+     * @param integrationTiming as defined in the integrationTime_t enum class
+    */
+    void SetIntegrationTiming(integrationTime_t integrationTiming);
+
+    enum class persProt_t
+    {
+        ALS_PERS_1 = 0,
+        ALS_PERS_2 = 1,
+        ALS_PERS_4 = 2,
+        ALS_PERS_8 = 3
+    };
+
+    /**
+     * @brief Set the persistence protect number setting as defined 
+     * @param protNumber as defined in the persProt_t enum class
+    */
+    void SetPersistenceProtect(persProt_t protNumber);
+
+    /**
+     * @brief Enable Interrupt that will be set the ALS_IF_L or ALS_IF_H bits when the programmed number of measurements (ALS_PERS) stay above/below the set threshold.
+     * @param enable True to enable interrupt. False to disable it 
+    */
+    void EnableInterrupt(bool enable);
+
+    /**
+     * @brief Set a high threshold that will be used in case the interrupt setting is enabled
+     * @param highThreshold Max threshold in lux 
+    */
+    void SetHighLimit(uint16_t highThreshold);
+
+    /**
+     * @brief Set a low threshold that will be used in case the interrupt setting is enabled
+     * @param lowThreshold Min threshold in lux 
+    */
+    void SetLowLimit(uint16_t lowThreshold);
+private:
+
+    uint16_t ReadRegister(const uint8_t *regAddr);
+
+    /* Send a 16-bit value as 2 8-bit values */
+    void     WriteRegister(const uint8_t *regAddr, uint16_t value);
+
+    uint16_t ALS_CONF_0_REG = 0; // Hold the current config for the als 
+
+    static constexpr uint8_t ALS_CONF_0_COMMAND = 0x00;
+    static constexpr uint8_t ALS_WH_COMMAND = 0x01;
+    static constexpr uint8_t ALS_WL_COMMAND = 0x02;
+    static constexpr uint8_t POWER_SAVING_COMMAND = 0x03;
+    static constexpr uint8_t ALS_COMMAND = 0x04;
+    static constexpr uint8_t WHITE_COMMAND = 0x05;
+    static constexpr uint8_t ALS_INT_COMMAND = 0x06;
+
+    // ALSConfigReg_t m_alsConfig;
+
+    i2c_inst_t *m_i2cX = nullptr;
+    uint8_t m_deviceAddress = 0x10;
+    uint8_t m_rxBuffer[2] = {0};
+
+    uint32_t m_defaultTimeout = 100000; // 100 ms is the default timeout for I2C messages
+};
+
+#endif /* VEML770_H */
